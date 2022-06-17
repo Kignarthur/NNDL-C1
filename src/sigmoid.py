Fix sigmoid sign

src/sigmoid.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def σ(z):
     """ Sigmoid function"""
-    return 1/(1 + np.exp(z))
+    return 1/(1 + np.exp(-z))
 
 def σp(z):
     """ Sigmoid first derivative"""