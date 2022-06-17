d1269d2c7f6fb7128f8cf82f9cbfe0a586e7f413
Arthur <amarthur@usp.br>
2022-06-16 23:16:49 -0300

    Add backpropagation method to network

--- src/network.py
+++ src/network.py
@@ -1,24 +1,66 @@
 import random
 import numpy as np
-import sigmoid
+from sigmoid import σ, σp
 
 class Network:
 
-    def __init__(self, sizes):
-        self.sizes = sizes
-        self.num_layers = len(sizes)
-        self.biases  = [np.random.randn(y, 1) for y in sizes[1:]]
-        self.weights = [np.random.randn(y, x) for y,x in zip(sizes[1:], sizes[:-1])]
-        """ Create the pairs (Weights,Nodes) for each layer except the last one.
-            Create matrices with dimension [Weights x Nodes] and randomize its values.
+    def __init__(self, layers):
+        self.layers  = layers
+        self.biases  = [np.random.randn(bias, 1) for bias in layers[1:]]
+        self.weights = [np.random.randn(weights, node) for weights,node in zip(layers[1:], layers[:-1])]
+        """ Create the pairs (Weights,Node) for each layer except the last one.
+            Create matrices with dimension [Weights x Node] and randomize its values.
             Each column of the matrix has all the weights of a single node.
         """
+        self.num_layers = len(layers)
+
+        # Store values to backpropagate
+        self.zs = []
+        self.activations = []
 
     def feedforward(self, x):
-        """ The activation value of a single node in the next layer is equal to σ(Σ wk*xk + bj).
-            Therefore to obtain the vector of activations we multiply the
-            vector a by the weight matrix W, and add the vector b of biases.
+        """ Return the output of the network for the input a_l."""
+        """ The activation value of a single node in the next layer is equal to σ(Σ w_jk*x_k + b_j).
+            Therefore to obtain the vector of activations we multiply the vector a_l by the weight
+            matrix W, and add the vector b of biases. We then apply the function σ elementwise.
         """
+        self.activations = [x]
+
+        a_l = x
         for W,b in zip(self.weights, self.bias):
-            x = sigmoid.sigmoid(np.dot(W,x) + b)
-        return x
+            z = np.dot(W, a_l) + b
+            a_l = σ(z)
+
+            self.zs.append(z)
+            self.activations.append(a_l)
+
+        return a_l
+
+    def backpropagate(self, x, y):
+        nabla_b = [np.zeros(b.shape) for b in self.biases]
+        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        L = self.num_layers - 1
+
+        # Feed forward
+        self.feedforward(x)
+
+        # Compute δ of the last layer L
+        z_L = self.zs[L]
+        a_L = self.activations[L]
+        δ = self.cost_derivative(a_L, y) * σp(z_L)
+
+        # Compute gradient for the cost function C_x
+        nabla_b[L] = δ
+        nabla_w[L] = np.dot(δ, self.activations[L-1].T)
+
+        # Backpropagate
+        for l in reversed(range(1, L)):
+            δ = np.dot(self.weights[l].T, δ) * σp(self.zs[l])
+
+            nabla_b[l] = δ
+            nabla_w[l] = np.dot(δ, self.activations[l-1].T)
+
+        return (nabla_b, nabla_w)
+
+    def cost_derivative(self, network_output, y):
+        return (network_output - y)
