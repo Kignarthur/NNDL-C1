a2aaf5a256d2505620282d3a252a5809ba8723e2
Arthur <wuubos@outlook.com>
2022-07-28 12:54:16 -0300

    Format main function code

--- src/main.py
+++ src/main.py
@@ -1,15 +1,21 @@
 import network
 import mnist_loader
 
+
 def main():
     layers = [784, 30, 10]
     net = network.Network(layers)
 
     training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
-    epc = 5; mbs = 10; eta = 3.0
+    epc = 5
+    mbs = 10
+    eta = 3.0
 
-    net.stochastic_gradient_descent(training_data=training_data, epochs=epc,
-                                    mini_batch_size=mbs, learning_rate=eta,
+    net.stochastic_gradient_descent(training_data=training_data,
+                                    epochs=epc,
+                                    mini_batch_size=mbs,
+                                    learning_rate=eta,
                                     test_data=test_data)
+
 
 main()
